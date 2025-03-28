d533475bd2b9367c0e16f27af27d39de7dc8d3a3
danielft2 <danielalmeida808z@gmail.com>
2025-03-28 15:54:41 -0300

    chore(frontend): modifier app route

--- app/frontend/src/profiles/researcher/modules/automatic-evaluations/components/list/evaluation-preview.tsx
+++ app/frontend/src/profiles/researcher/modules/automatic-evaluations/components/list/evaluation-preview.tsx
@@ -21,7 +21,7 @@ export function EvaluationCard({ data }: AutomaticEvaluationCardProps) {
 
   return (
     <div className="block rounded-lg border overflow-hidden border-slate-300 min-w-[400px] flex-1 font-heading relative">
-      <Link href={`work/evaluations/${data.id}`}>
+      <Link href={`work/${data.id}`}>
         <div className="bg-slate-100 space-y-4 p-4">
           <div className="flex items-center justify-between">
             <h1 className="font-heading -tracking-wider text-base font-medium">
